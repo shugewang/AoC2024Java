62f510458e34c0f796b217652f2181c1f5e1a73d
Shu Wang <shu.wang@autotrader.co.uk>
2024-12-14 15:14:33 +0000

    Tiny refactor

--- src/main/java/aoc/day01/Day02.java
+++ src/main/java/aoc/day01/Day02.java
@@ -14,16 +14,12 @@ public class Day02 extends Day {
 
     @Override
     public String part1(List<String> input) {
-//        System.out.println(input);
         Integer totalSafe = 0;
         for (String line : input) {
             List<Integer> levelList = Arrays.stream(line.split(" "))
                 .map(Integer::valueOf)
-                .collect(Collectors.toList());
-            System.out.println(levelList);
+                .toList();
 
-//            Boolean increasing = null;
-//            Boolean withinLimit = null;
             List<Boolean> increasingList = new ArrayList<>();
             List<Boolean> withinLimitList = new ArrayList<>();
             for (int i = 0; i < levelList.size()-1; i++) {
@@ -33,25 +29,24 @@ public class Day02 extends Day {
                 } else if (levelList.get(i + 1) - levelList.get(i) >= 4) {
                     increasingList.add(true);
                     withinLimitList.add(false);
+                    break;
                 } else if (levelList.get(i) - levelList.get(i+1) > 0 && levelList.get(i) - levelList.get(i+1) < 4) {
                     increasingList.add(false);
                     withinLimitList.add(true);
                 } else {
                     increasingList.add(false);
                     withinLimitList.add(false);
+                    break;
                 }
             }
-                System.out.println("increasing?:" + increasingList);
-                System.out.println("withinLimit?:" + withinLimitList);
-                long falseCount = increasingList.stream()
+
+            long falseCount = increasingList.stream()
                     .filter(b -> !b)
                     .count();
-                if (withinLimitList.contains(false) || falseCount > 0 && falseCount < withinLimitList.size()) {
-                    System.out.println("Not safe");
-                } else {
-                    System.out.println("Safe");
-                    totalSafe += 1;
-                }
+
+            if (!withinLimitList.contains(false) && (falseCount <= 0 || falseCount >= withinLimitList.size())) {
+                totalSafe += 1;
+            }
         }
         return totalSafe.toString();
     }
