6ebd89e91ef832dd845e0c0a18b2fb678b16c6ff
Shu Wang <shu.wang@autotrader.co.uk>
2024-12-14 12:40:51 +0000

    Add day 1 part 1 solution

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -2,7 +2,10 @@ package aoc.day01;
 
 import aoc.Day;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day01 extends Day {
 
@@ -12,6 +15,27 @@ public class Day01 extends Day {
 
     @Override
     public String part1(List<String> input) {
+//        List<List<Integer>> pairs = new ArrayList<>();
+        ArrayList<Integer> listA = new ArrayList<>();
+        ArrayList<Integer> listB = new ArrayList<>();
+        for (String line:input) {
+            String[] splitList = line.split("(\\D)", 2);
+            listA.add(Integer.valueOf(splitList[0]));
+            listB.add(Integer.valueOf(splitList[1].trim()));
+        }
+
+        Collections.sort(listA);
+        Collections.sort(listB);
+
+        Integer total = 0;
+        for (int i = 0; i < listA.size(); i++) {
+            total += Math.abs(listA.get(i) - listB.get(i));
+        }
+
+        System.out.println(listA);
+        System.out.println(listB);
+        System.out.println(total);
+
         return input.isEmpty() ? "" : input.get(0);
     }
 
